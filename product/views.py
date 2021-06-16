730fe560b9d0a3a0d810e7c2160a65d5793deada
kairoboss <kairoboss125@gmail.com>
2021-06-16 18:21:10 +0600

    remove halfCRUD of product for using ViewSets

--- product/views.py
+++ product/views.py
@@ -4,17 +4,3 @@ from rest_framework.pagination import PageNumberPagination
 
 from product import serializers
 from product.models import Product
-
-
-class StandardResultSetPagination(PageNumberPagination):
-    page_size = 100
-    page_size_query_param = 'page_size'
-    max_page_size = 1000
-
-
-class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = serializers.ProductSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-    pagination_class = StandardResultSetPagination
-    # filter_backends =
