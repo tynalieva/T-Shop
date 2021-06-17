cb2d7e3a8d076980495ea58b2c00412ec7a1b4d2
tynalieva <zaida.tynalieva@gmail.com>
2021-06-17 17:59:45 +0600

    add images, categories

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from product.models import Product
-
+from product.models import Product, Category, ProductImage
 
 admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Category)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from user.models import CustomUser
+
 
 class DataABC(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,12 +11,30 @@ class DataABC(models.Model):
         abstract = True
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True,blank=True, related_name='children')
+
+    def __str__(self):
+        if not self.parent:
+            return f"{self.name}"
+        else:
+            return f"{self.parent} -->{self.name}"
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
 class Product(DataABC):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     quantity = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='images/', blank=True)
+    user = models.ForeignKey(CustomUser, related_name='products', on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True)
 
 
 class ProductImage(models.Model):
