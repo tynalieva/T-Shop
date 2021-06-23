4d3abe6eb64b9127d3b66a7ad2e7dde4ee5e9904
tynalieva <zaida.tynalieva@gmail.com>
2021-06-23 08:47:42 +0600

    ckeck

--- user/views.py
+++ user/views.py
@@ -1,8 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
-
-# Create your views here.
-from django.views import View
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
